69f1ff03f7c3e43d5db6e70275e93b5cc18e0ffa
flopwzh <phillipwzh@gmail.com>
2026-04-22 16:45:33 -0400

    reverse selfplay_1 script to default

--- example_ray_ma_players.py
+++ example_ray_ma_players.py
@@ -5,11 +5,11 @@ from soccer_twos import EnvType
 from utils import create_rllib_env
 
 
-NUM_ENVS_PER_WORKER = 1
+NUM_ENVS_PER_WORKER = 3
 
 
 if __name__ == "__main__":
-    ray.init(include_dashboard=False)
+    ray.init()
 
     tune.registry.register_env("Soccer", create_rllib_env)
     temp_env = create_rllib_env({"variation": EnvType.multiagent_player})
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         config={
             # system settings
             "num_gpus": 0,
-            "num_workers": 1,
+            "num_workers": 8,
             "num_envs_per_worker": NUM_ENVS_PER_WORKER,
             "log_level": "INFO",
             "framework": "torch",
@@ -40,19 +40,12 @@ if __name__ == "__main__":
                 "num_envs_per_worker": NUM_ENVS_PER_WORKER,
                 "variation": EnvType.multiagent_player,
             },
-            "model":{
-                "fcnet_hiddens": [512, 512],
-                "fcnet_activation": "relu",
-                "vf_share_layers": True,
-            },
-            "rollout_fragment_length": 500,
-            "train_batch_size": 12000,
         },
         stop={
             "timesteps_total": 15000000,  # 15M
             # "time_total_s": 14400, # 4h
         },
-        checkpoint_freq=100,
+        checkpoint_freq=50,
         checkpoint_at_end=True,
         local_dir="./ray_results",
         # restore="./ray_results/PPO_selfplay_1/PPO_Soccer_ID/checkpoint_00X/checkpoint-X",
